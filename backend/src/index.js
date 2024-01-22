21cbdd8748022afd929164af0819eff70fa3feaf
Aastha Mahindra <155626319+aasthamahindra@users.noreply.github.com>
2024-01-22 17:16:53 +0530

    index.js: server added

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,60 +1,51 @@
 const express = require('express');
-const { ApolloServer } = require('apollo-server');
+const { ApolloServer } = require('apollo-server-express');
 const { createServer } = require('http');
-const { expressMiddleware } = require('@apollo/server/express4');
 const { typeDefs } = require('./schema/typedefs');
 const { resolvers } = require('./resolvers/resolvers');
-const cors = require('cors');
 const app = express();
+const { verifyToken } = require('./authorization/jwt.verify');
 
-// async function startServer() {
-//     const server = new ApolloServer({ typeDefs, resolvers });
-  
-//     await server.start();
-  
-//     app.use('/', cors(), express.json(), expressMiddleware(server));
-  
-//     await new Promise((resolve) =>
-//       app.listen({ port: 5000, host: '0.0.0.0' }, resolve)
-//     ).then((d) => {
-//       console.log('Server started on port 5000');
-//     }).catch((e) => {
-//       console.log(`Error: ${e}`);
-//     });
-  
-//     app.get('/health', (req, res) => {
-//       res.status(200).send('Health check successful!');
-//     });
-//   }
-  
-//   startServer();
+const startServer = async () => {
+    const server = new ApolloServer({
+        typeDefs,
+        resolvers,
+        context: ({ req }) => {
+          if (req.headers && req.headers.authorization) {
+            const auth = req.headers.authorization;
+            const split = auth.split(" ");
+            const bearer = split[0];
+            const token = split[1];
+            if (bearer == "Bearer") {
+                const user = verifyToken(token);
+                if (user.error) {
+                    return { error: user.error, message: user.msg }
+                } else {
+                    return { user }
+                }
+            } else {
+                return { error: 'Authentication must use Bearer.' }
+            }
+          } else {
+            return { error: 'User must be authenticated.' }
+          }
+        },
+        playground: true,
+    });
+    
+    await server.start();
 
-const server = new ApolloServer({ 
-  typeDefs,
-  resolvers,
-  context: ({ req }) => {
-    if (req.headers && req.headers.authorization) {
-      const auth = req.headers.authorization;
-      const parts = auth.split(' ');
-      const bearer = parts[0];
-      const token = parts[1];
+    // applyMiddleware() apply to be a middleware in Express to setup route
+    server.applyMiddleware({ app });
+    
+    const httpServer = createServer(app);
+    httpServer.listen(5000, '0.0.0.0', () => {
+        try {
+            console.log('Server started at port 5000');
+        } catch (error) {
+            console.error(`Failed to start server: ${error}`);
+        }
+    });
+}
 
-      if (bearer === "Bearer") {
-        const user = get
-      }
-    }
-  }
-});
-
-server.applyMiddleware({ app });
-
-const httpServer = createServer(app);
-
-httpServer.listen({ port: 5000, host: '0.0.0.0' }, () => {
-  try {
-    console.log('Server started at port 5000');
-  } catch (error) {
-    console.log(`Error starting server: ${error}`);
-  }
-})
-  
+startServer();
