f8ef8aa590303c0c197bddb41d93c0f76aa1f5a7
Aastha Mahindra <155626319+aasthamahindra@users.noreply.github.com>
2024-02-07 11:55:46 +0530

    Update typedefs.js

--- backend/src/schema/typedefs.js
+++ backend/src/schema/typedefs.js
@@ -1,22 +1,40 @@
 const gql = require('graphql-tag');
 
 const typeDefs = gql`
-    type Query {
-        getMovies: [Movie]
-    }
-    type Mutation {
-        addMovie(name: String!, genre: String!, image: String!, year: String): Movie
-        login(email: String!, password: String!): Tokens
-    }
     type Movie {
         name: String,
         genre: String,
         year: String,
         image: String
+        error: String
     }
-    type Tokens {
-        accessToken: String
-        refreshToken: String
+
+    type User {
+        username: String
+        email: String
+        password: String
+        role: String
+    }
+
+    type SignInResponse {
+        token: String
+        error: String
+    }
+
+    type SignUpResponse {
+        token: String
+        error: String
+    }
+
+    type Query {
+        getMovies: [Movie],
+        getUsers: [User] 
+    }
+
+    type Mutation {
+        addMovie(name: String!, genre: String!, image: String!, year: String): Movie
+        signUp(email: String!, username: String, password: String!, role: String): SignUpResponse
+        signIn(email: String!, password: String!): SignInResponse
     }
 `;
 
