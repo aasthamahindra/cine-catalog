da8439f79a63f786508552888d309d1732b06fa8
Aastha Mahindra <155626319+aasthamahindra@users.noreply.github.com>
2024-02-07 10:28:24 +0530

    Update resolvers.js

--- backend/src/resolvers/resolvers.js
+++ backend/src/resolvers/resolvers.js
@@ -1,26 +1,104 @@
-const { model } = require('../models/movie.models');
+require('dotenv').config();
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { movieModel } = require('../models/movie.models');
+const { userModel } = require('../models/user.models')
 
 const resolvers = {
     Query: {
         getMovies: async (parent, args) => {
-            const movies = await model.find({});
+            const movies = await movieModel.find({});
             if (movies.length > 0) {
                 return movies;
             } else {
                 return [];
             } 
         },
+
+        getUsers: async () => {
+            const user = await userModel.find({});
+            return user;
+        }
     },
     Mutation: {
-        addMovie: (parent, args) => {
-            let movies = new model({
-                name: args.name,
-                genre: args.genre,
-                year: args.year,
-                image: args.image,
+        addMovie: async (parent, args, context, info) => {
+            const { name, genre, year, image} = args;
+
+            const { id } = context.user;
+            const user = await userModel.findOne({ _id: Object(id) });
+            const role = user.role;
+
+            if (role !== "admin" && role !== "editor")
+            return {
+                error: "You have no permission to perform this action",
+            };
+
+            let movies = new movieModel({
+                name,
+                genre,
+                year,
+                image,
             })
             movies.save();
             return movies;
+        },
+
+        signIn: async (parent, args, context, info) => {
+            const { email, password } = args;
+            const user = await userModel.findOne({ email });
+
+            // if no user is found
+            if (!user) {
+                return { error: "User doesn't exist! Please sign up." }
+            } else {
+                // check password: if it doesn't match throw authentication error
+                const validate = await bcrypt.compare(password, user.password).then((result) => {
+                    return result;
+                }).catch(e => { return { error: e} } );
+                if (!validate) {
+                    return { error: "Password doesn't match!" };
+                } else {
+                    return {
+                        token: jwt.sign(
+                            { id: user._id },
+                            process.env.SECRET,
+                        )
+                    }
+                }
+            }
+        },
+
+        signUp: async (parent, args, context, info) => {
+            const { email, password, username } = args;
+            let { role } = args;
+            const existingUser = await userModel.findOne({ email });
+
+            if (existingUser) {
+                return { error: 'User already exists! Please sign in.'};
+            } else {
+                const salt = await bcrypt.genSalt(10);
+                const hashPassword = await bcrypt.hash(password, salt);
+
+                if (!role) {
+                    role = 'reader';
+                };
+                
+                const user = new userModel({
+                    email,
+                    username,
+                    role,
+                    password: hashPassword
+                });
+                user.save();
+                return {
+                    message: 'User created successfully',
+                    user,
+                    token: jwt.sign(
+                        { id: user._id },
+                        process.env.SECRET,
+                    )
+                }
+            }
         },
     },
 };
